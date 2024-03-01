f643ec33f4f0a61900fbda27ed97341f2229d224
Disha2901185 <dishakansal2410@gmail.com>
2024-03-01 11:05:11 +0530

    sub-categories UI

--- src/components/onBoarding/subCategory.js
+++ src/components/onBoarding/subCategory.js
@@ -13,6 +13,7 @@ import {COLORS} from '../../assets/theme';
 import {SafeAreaView} from 'react-native-safe-area-context';
 import {ScrollView} from 'react-native-virtualized-view';
 import {useNavigation} from '@react-navigation/native';
+import Button from '../Common/Button';
 
 const SubcategoryScreen = ({route}) => {
   const {subCategories} = route.params;
@@ -45,35 +46,37 @@ const SubcategoryScreen = ({route}) => {
         tittle={'SELECT A CATEGORY'}
         onPress={() => navigation.goBack()}
       />
-      <View style={{marginVertical: 25}}>
+      <View style={styles.subcategoriesContainer}>
         <ScrollView>
-          <FlatList
-            data={subCategories}
-            renderItem={({item}) => (
-              <View style={styles.section}>
-                <CheckBox
-                  tintColors={{true: COLORS.white, false: 'white'}}
-                  value={selectedSubcategories.includes(item)}
-                  onValueChange={() => toggleSubcategory(item)}
-                />
-                <Text
-                  style={{
-                    color: COLORS.white,
-                    fontSize: 18,
-                    paddingHorizontal: 10,
-                  }}>
-                  {item}
-                </Text>
-              </View>
-            )}
-            keyExtractor={(item, index) => index.toString()}
-          />
+          <View style={styles.subcategoryItems}>
+            <FlatList
+              data={subCategories}
+              renderItem={({item}) => (
+                <View style={styles.section}>
+                  <CheckBox
+                    tintColors={{true: COLORS.darkBlue, false: COLORS.darkBlue}}
+                    value={selectedSubcategories.includes(item)}
+                    onValueChange={() => toggleSubcategory(item)}
+                  />
+                  <Text
+                    style={{
+                      color: COLORS.darkBlue,
+                      fontSize: 14,
+                      paddingHorizontal: 8,
+                      fontWeight: 'bold',
+                    }}>
+                    {item}
+                  </Text>
+                </View>
+              )}
+              keyExtractor={(item, index) => index.toString()}
+              numColumns={2}
+            />
+          </View>
         </ScrollView>
-        <TouchableOpacity onPress={saveSelection} style={styles.button}>
-          <Text style={{fontWeight: '600', color: 'black'}}>
-            Save Selection
-          </Text>
-        </TouchableOpacity>
+      </View>
+      <View style={styles.buttonContainer}>
+        <Button tittle={'continue'} onPress={saveSelection} />
       </View>
     </SafeAreaView>
   );
@@ -84,26 +87,37 @@ export default SubcategoryScreen;
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#202020',
     justifyContent: 'flex-start',
   },
-  button: {
-    height: 45,
-    backgroundColor: '#89b9ff',
-    borderRadius: 8,
-    alignItems: 'center',
-    justifyContent: 'center',
-    width: '90%',
-    alignSelf: 'center',
-    marginTop: 10,
-  },
+
   section: {
+    display: 'flex',
     flexDirection: 'row',
     alignItems: 'center',
-    marginVertical: 5,
+
+    boxSizing: 'border-box',
+    marginVertical: 10,
     marginHorizontal: 10,
-    padding: 15,
-    backgroundColor: '#082e66',
-    borderRadius: 8,
+    paddingVertical: 10,
+    paddingHorizontal: 4,
+    backgroundColor: COLORS.secondaryButtonColor,
+    borderRadius: 50,
+    display: 'inline',
+    width: '45%',
+  },
+  subcategoryItems: {
+    display: 'flex',
+    flexDirection: 'row',
+    flexWrap: 'wrap',
+    justifyContent: 'space-between',
+    marginTop: 10,
+    width: '100%',
+  },
+  subcategoriesContainer: {
+    marginTop: 25,
+    display: 'flex',
+    flexWrap: 'wrap',
+    flexDirection: 'column',
+    marginHorizontal: 15,
   },
 });
