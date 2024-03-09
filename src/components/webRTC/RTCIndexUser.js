ab68083571a952119f42af750fb5af8f4ff3451e
Himanshu Lal <himanshulal56@gmail.com>
2024-03-10 00:09:21 +0530

    autoJoinCallTestingDone

--- src/components/webRTC/RTCIndexUser.js
+++ src/components/webRTC/RTCIndexUser.js
@@ -20,19 +20,29 @@ import {
 import {useState} from 'react';
 
 import firestore from '@react-native-firebase/firestore';
-import {useDispatch, useSelector} from 'react-redux';
-import {addChannelId} from '../../redux/callingChannelSlice';
+import {Dimensions} from 'react-native';
+import {COLORS} from '../../assets/theme';
+import VectorIcon from '../../utils/VectorIcon';
+import {useSelector} from 'react-redux';
+const {width, height} = Dimensions.get('window');
+const RTCIndex = ({route, navigation}) => {
+  const userUID = useSelector(state => state.userToken.UID);
+  console.log('🚀 ~ RTCIndex ~ userUID:', userUID);
 
-const RTCIndex = ({navigation}) => {
+  const {clickedSellerDeviceToken} = route.params;
+  console.log(
+    '🚀 ~ RTCIndex ~ clickedSellerDeviceToken:',
+    clickedSellerDeviceToken,
+  );
+
   const [remoteStream, setRemoteStream] = useState(null);
-  const currentChannelId = useSelector(state => state.callingChannel.value);
-  console.log('🚀 ~ RTCIndex ~ currentChannelId:', currentChannelId);
 
   const [webcamStarted, setWebcamStarted] = useState(false);
   const [localStream, setLocalStream] = useState(null);
   const [channelId, setChannelId] = useState(null);
+  //  const [clickedSellerDeviceToken, setClickedSellerDeviceToken] =
+  //    useState(null);
   const pc = useRef();
-  const dispatch = useDispatch();
   const servers = {
     iceServers: [
       {
@@ -51,8 +61,6 @@ const RTCIndex = ({navigation}) => {
         video: true,
         audio: true,
       });
-
-      console.log('Local Stream:', localStream);
 
       setLocalStream(localStream);
 
@@ -75,9 +83,73 @@ const RTCIndex = ({navigation}) => {
       console.error('Error starting webcam:', error);
     }
   };
-  // useEffect(() => {
-  //   startWebcam();
-  // }, []);
+  // async function setDeviceToken(itemId) {
+  //   await firestore()
+  //     .collection('Sellers')
+  //     .doc(itemId)
+  //     .get()
+  //     .then(doc => {
+  //       if (doc.exists) {
+  //         // Document data is available in doc.data()
+  //         const sellerData = doc.data();
+  //         // console.log('Seller Data:', sellerData.deviceToken);
+  //         // sellerFcmToken = sellerData.deviceToken;
+  //         setClickedSellerDeviceToken(sellerData.deviceToken);
+  //       } else {
+  //         console.log('No such document!');
+  //       }
+  //     })
+  //     .catch(error => {
+  //       console.error('Error getting seller document:', error);
+  //     });
+  // }
+  async function handleCallNotification(channelId) {
+    console.log('<==========handleCallNotification==========>');
+    console.log('clickedSellerDeviceToken--->', clickedSellerDeviceToken);
+    console.log('channelId---->', channelId);
+    // const message = {
+    //   to: clickedSellerDeviceToken,
+    //   notification: {
+    //     title: '📲Fasto user Calling',
+    //     body: '📞📞Call from a fasto user📞📞',
+    //   },
+    //   data: {
+    //     // You can include additional data if needed
+    //     // ...
+    //     channelId: channelId,
+    //   },
+    // };
+    const message = {
+      to: clickedSellerDeviceToken,
+      notification: {
+        title: '📲Fasto user Calling',
+        body: '📞📞Call from a fasto user📞📞',
+      },
+      data: {
+        channelId: `${channelId}`,
+        userUID: userUID,
+        // Add more key-value pairs as needed
+      },
+    };
+
+    console.log(
+      'handleCallNotificationClickedSellerDeviceToken----------->',
+      clickedSellerDeviceToken,
+    );
+    await fetch('https://fcm.googleapis.com/fcm/send', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization:
+          'key=AAAArsiGfCg:APA91bG4MQ_kSTeuCFZDjEkStvHn_zBJ_WmyTLzUg9C7sPmy3THk7s8XnoyhSjrhZ6X_X7VRGPpO_yCFXJ2AYYUEPUWoPV6Lm7jZ28BQ4mQKeoDM8SsrgnE73VdfelwDG9S9ywP5La8F', // Replace with your server key
+      },
+      body: JSON.stringify(message),
+    });
+  }
+
+  useEffect(() => {
+    startWebcam();
+  }, []);
   const startCall = async () => {
     const channelDoc = firestore().collection('channels').doc();
     const offerCandidates = channelDoc.collection('offerCandidates');
@@ -112,6 +184,7 @@ const RTCIndex = ({navigation}) => {
         }
       });
     });
+    await handleCallNotification(channelDoc.id);
   };
 
   const joinCall = async () => {
@@ -148,10 +221,6 @@ const RTCIndex = ({navigation}) => {
       });
     });
   };
-  console.log(remoteStream?.toURL());
-  console.log('🚀 ~ RTCIndex ~ remoteStream:', remoteStream);
-  console.log(localStream?.toURL());
-  console.log('🚀 ~ RTCIndex ~ localStream:', localStream);
   const endCall = () => {
     // Close the peer connection and reset states
     if (pc.current) {
@@ -161,36 +230,32 @@ const RTCIndex = ({navigation}) => {
     setRemoteStream(null);
     setChannelId(null);
     setWebcamStarted(false);
-    dispatch(addChannelId(null));
-    navigation.navigate('OnBoardScreen');
+    navigation.goBack();
   };
-  setTimeout(function () {
-    joinCall();
-  }, 100);
-  useEffect(() => {
-    console.log('currentChannelId', currentChannelId);
-    setChannelId(currentChannelId);
-    startWebcam();
-  }, []);
   return (
     <KeyboardAvoidingView style={styles.body} behavior="position">
-      <SafeAreaView>
+      <SafeAreaView style={styles.container}>
         {localStream && (
-          <RTCView
-            streamURL={localStream?.toURL()}
-            style={styles.stream}
-            objectFit="cover"
-            mirror
-          />
+          <View
+            style={remoteStream ? styles.joined : styles.localStreamContainer}>
+            <RTCView
+              streamURL={localStream?.toURL()}
+              style={styles.stream}
+              objectFit="cover"
+              mirror
+            />
+          </View>
         )}
 
         {remoteStream && (
-          <RTCView
-            streamURL={remoteStream?.toURL()}
-            style={styles.RemoteStream}
-            objectFit="cover"
-            mirror
-          />
+          <View style={styles.remoteStreamContainer}>
+            <RTCView
+              streamURL={remoteStream?.toURL()}
+              style={styles.RemoteStream}
+              objectFit="cover"
+              mirror
+            />
+          </View>
         )}
         <View style={styles.buttons}>
           {/* {!webcamStarted && (
@@ -198,16 +263,27 @@ const RTCIndex = ({navigation}) => {
           )} */}
           {webcamStarted && <Button title="Start call" onPress={startCall} />}
           {webcamStarted && (
-            <View style={{flexDirection: 'row'}}>
+            <View
+              style={{
+                flexDirection: 'row',
+                marginBottom: 10,
+                alignSelf: 'center',
+              }}>
               <Button title="Join call" onPress={joinCall} />
               <TextInput
                 value={channelId}
                 placeholder="callId"
                 minLength={45}
                 style={{borderWidth: 1, padding: 5}}
-                // onChangeText={newText => setChannelId(newText)}
+                onChangeText={newText => setChannelId(newText)}
               />
-              <Button title="End Call" onPress={endCall} />
+              <VectorIcon
+                name={'closecircle'}
+                type={'AntDesign'}
+                size={50}
+                color={'red'}
+                onPress={endCall}
+              />
             </View>
           )}
         </View>
@@ -218,23 +294,49 @@ const RTCIndex = ({navigation}) => {
 
 const styles = StyleSheet.create({
   body: {
-    backgroundColor: '#fff',
-
+    backgroundColor: COLORS.secondaryButtonColor,
+    flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
-    ...StyleSheet.absoluteFill,
+    width: width,
+    height: height,
+  },
+  container: {
+    width: width,
+    height: height,
+    zIndex: 1,
+    marginTop: width * 0.05,
+  },
+  joined: {
+    flex: 1,
+    margin: 10,
+    width: width * 0.95,
+    height: height,
+    borderRadius: 18,
+    overflow: 'hidden',
+  },
+  localStreamContainer: {
+    flex: 1,
+    margin: 10,
+    borderRadius: 15,
+    width: width * 0.95,
+    height: height,
+    borderRadius: 18,
+    overflow: 'hidden',
   },
   stream: {
-    flex: 2,
-    width: 100,
-    height: 100,
+    flex: 1,
+  },
+  remoteStreamContainer: {
+    flex: 1,
     margin: 10,
+    width: width * 0.95,
+    height: height,
+    borderRadius: 18,
+    overflow: 'hidden',
   },
   RemoteStream: {
-    flex: 2,
-    width: 200,
-    height: 200,
-    margin: 10,
+    flex: 1,
   },
   buttons: {
     alignItems: 'flex-start',
